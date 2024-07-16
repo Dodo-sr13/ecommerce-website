72015ed7f773010bf0e49da2f75e369ff52ac9e3
Dodo-sr13 <soumyadeep1311@gmail.com>
2024-07-16 13:49:13 +0530

    Added constants file

---
+++ client/src/constants/index.js
@@ -0,0 +1,4 @@
+export const REACT_APP_API_URL = "https://ecommerce-website-s7te.onrender.com";
+export const API_URL = "http://localhost:8080";
+export const STRIPE_API_KEY = "pk_test_51P1CjkSA31U9mjxylrzBYh4rV91adyh087gsf5JcN0lCFEnsRoeIXEZOzZcJHPs26tGZjohOBB9sHNY2xNrefLkH008bJ4YXpX";
+export const NODE_ENV = "deploy";
